d9ac4c10d239eae8d05788a75dc15a99611eb7dd
mayaakhriza <37297131+mayaakhriza@users.noreply.github.com>
2018-05-14 08:32:57 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$host = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$db = "login"; 
-$koneksi = mysqli_connect($host, $username, $password) or die ("Koneksi Gagal!"); 
-mysqli_select_db($koneksi, $db) or die ("database tidak tersedia"); 
-?>
